6ac879bd8d792a3dd72dd8c41c3d0eedbf72c791
MohebMak <m.makramallah@hotmail.com>
2018-02-09 20:21:23 +0200

    milestone1

---
+++ src/client.java
@@ -0,0 +1,39 @@
+import java.io.*; 
+import java.net.*; 
+class client { 
+
+    public static void main(String argv[]) throws Exception 
+    { 
+    	while(true)
+    	{
+        String sentence; 
+        String modifiedSentence; 
+
+        BufferedReader inFromUser = 
+          new BufferedReader(new InputStreamReader(System.in)); 
+
+        Socket clientSocket = new Socket("Dell", 6789); 
+
+        DataOutputStream outToServer = 
+          new DataOutputStream(clientSocket.getOutputStream()); 
+
+        BufferedReader inFromServer = 
+                new BufferedReader(new
+                InputStreamReader(clientSocket.getInputStream())); 
+
+              sentence = inFromUser.readLine(); 
+
+              outToServer.writeBytes(sentence + '\n'); 
+
+              modifiedSentence = inFromServer.readLine(); 
+
+              System.out.println("FROM SERVER: " + modifiedSentence); 
+
+              if(modifiedSentence.equals("BYE"))
+              {
+              clientSocket.close(); 
+              }
+    	}
+    	
+          } 
+      } 
